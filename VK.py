7321e2e976c0303b020c2d0d9c7c9705ace0b8f9
Troll <kam-adm@mail.ru>
2023-05-01 19:13:27 +0300

    courseworkv 1.1

--- VK.py
+++ VK.py
@@ -1,13 +1,16 @@
 import logging
 import requests
 import json
+from heapq import nlargest
 
 logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
                     format="%(asctime)s %(levelname)s %(message)s"
                     )
+
 class VK:
-   def __init__(self, access_token, user_id, version='5.131'):
-       self.token = access_token
+   def __init__(self, user_id=int(input('Введите id пользователя: ')), version='5.131'):
+       logging.info('Введён id пользователя.')
+       self.token = ''
        self.id = user_id
        self.version = version
        self.params = {'access_token': self.token, 'v': self.version}
@@ -16,35 +19,79 @@ class VK:
        url = 'https://api.vk.com/method/users.get'
        params = {'user_ids': self.id}
        response = requests.get(url, params={**self.params, **params})
-       if response.status_code == 200:
-           print('Информация о пользователе загружена!')
-           logging.info('Информация о пользователе загружена!')
-       else:
+       if response.status_code != 200:
            print('Инфоормация о пользователе не загружена.')
            logging.error('Инфоормация о пользователе не загружена.')
-       for e in response.json()['response']:
-           if e['first_name'] == 'DELETED':
-               print('Пользователь удалён!!!')
-               logging.error('Пользователь удалён!!! -- DELETED')
+           exit()
+       elif response.status_code == 200:
+           if len(response.json()['response']) == 0:
+               print("Введён неверный id!!!")
                exit()
-           return print("Владельцем данного id является: ", e['first_name'], e['last_name'] )
+           else:
+               print('Информация о пользователе загружена!')
+               logging.info('Информация о пользователе загружена!')
+               for status_user in response.json()['response']:
+                   if status_user['first_name'] == 'DELETED':
+                       print('Пользователь удалён!!!')
+                       logging.error('Пользователь удалён!!! -- DELETED')
+                       exit()
+                   return print("Владельцем данного id является: ", status_user['first_name'], status_user['last_name'])
 
 
    def vk_foto(self):
-       api = requests.get('https://api.vk.com/method/photos.getAll', params={
+       api = requests.get('https://api.vk.com/method/photos.get', params={
            'owner_id': self.id,
            'access_token': self.token,
            'foto_sizes': 1,
            'extended': 1,
            'count': 200,
-           'v': self.version
+           'v': self.version,
+           'album_id': 'profile'
        })
+       print(api.json())
+       if api.status_code != 200:
+           print('Данные не получены, запрос завершился с кодом', api.status_code)
+           exit()
+       print(api.status_code)
        return json.loads(api.text)
+   def vk_check(self):
+       if vk.vk_foto()['response']['count'] == 0:
+           print("У пользователя нет фото!")
+           logging.error("У пользователя нет фото!")
+           exit()
+       if len(vk.vk_foto()['response']['items']) == 0:
+            print("В ответе от сервера нет необходимых данных")
+            logging.error("В ответе от сервера нет необходимых данных")
+            exit()
+       for metadata in vk.vk_foto()['response']['items']:
+           if len(metadata['sizes'][-1]['url']) == 0 and len(metadata['sizes'][-1]['type']) == 0 and len(metadata['sizes'][-1]['height']) == 0 and len(metadata['sizes'][-1]['width']) == 0:
+               print("В ответе от сервера нет необходимых данных")
+               logging.error("В ответе от сервера нет необходимых данных")
+               exit()
 
-
-
-access_token = ''
-user_id = int(input("Введите id пользователя:"))
-logging.info('Введён id пользователя.')
-vk = VK(access_token, user_id)
-vk.users_info()
+   def vk_foto_serch_and_sort(self, quantiti_foto=int(input("Введите колличество фото: "))):
+       vk.vk_check()
+       logging.info("Произведена проверка данных.")
+       logging.info('Введено колличество фото.')
+       dict_foto = {}
+       list_size = []
+       result_foto = {}
+       if vk.vk_foto()['response']['count'] < quantiti_foto:
+           print(f"У пользователя в альбоме всего {vk.vk_foto()['response']['count']} фото, это колличество и будет загруженно на яндекс диск!")
+       for metadata in vk.vk_foto()['response']['items']:
+           url_foto = metadata['sizes'][-1]['url']
+           likes_quantiti = metadata['likes']['count']
+           type_foto = metadata['sizes'][-1]['type']
+           size_foto = metadata['sizes'][-1]['height'] + metadata['sizes'][-1]['width']
+           dict_foto.update({url_foto: [likes_quantiti, type_foto, size_foto]})
+           list_size.append(size_foto)
+       for key, value in dict_foto.items():
+           for sort_foto in nlargest(quantiti_foto, list_size):
+               foto_count = 0
+               if sort_foto in value:
+                   result_foto.update({key: [value]})
+                   foto_count += 1
+           if quantiti_foto <= len(result_foto.values()):
+               break
+       return result_foto
+vk = VK()
--- YandexDisk.py
+++ YandexDisk.py
@@ -1,6 +1,7 @@
 import requests
 import logging
 
+
 logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
                      format="%(asctime)s %(levelname)s %(message)s"
                      )
@@ -21,17 +22,20 @@ class YandexDisk:
         file_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
         headers = self.get_headers()
         response = requests.get(file_url, headers=headers)
+        print(response.json())
         return response.json()
 
     def create_folder(self, path='vk_backup_foto'):
+        url = 'https://cloud-api.yandex.net/v1/disk/resources'
         headers = self.get_headers()
-        requests.put(f'{url}?path={path}', headers=headers)
-        return
+        params = {"overwrite": "true"}
+        requests.put(f'{url}?path={path}', headers=headers, params=params)
+
     def get_upload_link(self, disk_file_path):
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = self.get_headers()
         params = {"path": disk_file_path, "overwrite": "true"}
-        response = requests.get(upload_url, headers=headers,params=params)
+        response = requests.get(upload_url, headers=headers, params=params)
         if response.status_code == 200:
             print('Ключ яндекс диска успешно проверен!')
             logging.info('Ключ яндекс диска успешно проверен!')
@@ -44,14 +48,12 @@ class YandexDisk:
         href = data.get('href')
         return href
 
-    def upload_file_to_disk(self, disk_file_path, filename):
-        href = self.get_upload_link(disk_file_path=disk_file_path)
-        response = requests.put(href, data=open(filename, 'rb'))
-        if response.status_code != 201:
-            print("Изображение не загружено")
-            logging.error("Изображение не загружено")
+    def upload_file_to_disk(self, url, disk_file_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {"url": url, "path": disk_file_path, "overwrite": "true"}
+        response = requests.post(upload_url, headers=headers, params=params)
+        if response.status_code != 202:
+            print('Изображение не загружено, код ошибки:', response.status_code)
             exit()
-
-url = 'https://cloud-api.yandex.net/v1/disk/resources'
-
 ya = YandexDisk(token=TOKEN)
